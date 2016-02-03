a82ffd9d6eaa8b3e5fdd4afc8a617f0979df17da
Katerina Kampardi <kkampardi@gmail.com>
2016-02-03 15:11:32 +0200

    #facbook login initial design

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 angular
-  .module('ngsocialApp', ['ngResource', 'ui.router'])
+  .module('ngsocialApp', ['ngResource', 'ui.router', 'ngFacebook'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
     // route for the home page
@@ -21,4 +21,21 @@ angular
 
             })
             $urlRouterProvider.otherwise('/');
-  });
+  })
+  //facebook config
+  .config( function( $facebookProvider ) {
+  $facebookProvider.setAppId('164515627256520');
+  $facebookProvider.setPermissions("email","public_profile", "user_posts", "publish_actions", "user_photos");
+
+  })
+  //facebook run
+  .run( function( $rootScope ) {
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+  })
+;
