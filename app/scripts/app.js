f7404b3f2797bbc070a4b7caa9021091a9490da8
Katerina Kampardi <kkampardi@gmail.com>
2016-02-05 13:28:51 +0200

    #Implement Facebook Login

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,13 +19,13 @@ angular
                     }
                 }
 
-            })
+            });
             $urlRouterProvider.otherwise('/');
   })
   //facebook config
   .config( function( $facebookProvider ) {
   $facebookProvider.setAppId('164515627256520');
-  $facebookProvider.setPermissions("email","public_profile", "user_posts", "publish_actions", "user_photos");
+  $facebookProvider.setPermissions('email','public_profile', 'user_posts', 'publish_actions', 'user_photos');
 
   })
   //facebook run
@@ -34,7 +34,7 @@ angular
          var js, fjs = d.getElementsByTagName(s)[0];
          if (d.getElementById(id)) {return;}
          js = d.createElement(s); js.id = id;
-         js.src = "//connect.facebook.net/en_US/sdk.js";
+         js.src = '//connect.facebook.net/en_US/sdk.js';
          fjs.parentNode.insertBefore(js, fjs);
        }(document, 'script', 'facebook-jssdk'));
   })
--- app/scripts/controllers/facebook.js
+++ app/scripts/controllers/facebook.js
@@ -1,7 +1,15 @@
 'use strict';
 
 angular.module('ngsocialApp')
-  .controller('FacebookController', ['$scope', '$http', function ($scope, $http) {
+  .controller('FacebookController', ['$scope', '$facebook',
+    function ($scope, $facebook) {
     console.log('FacebookController');
+    $scope.islogedin = false;
+
+    $scope.login = function(){
+      $facebook.login().then(function(){
+        console.log('Logged in');
+      });
+    };
   }])
 ;
